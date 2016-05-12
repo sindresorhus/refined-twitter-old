18667139fda8e4ab23d1534ae03be2d876112d32
Dan Callahan <dan.callahan@gmail.com>
2016-05-12 00:08:36 -0500

    Revert "Spoof Chrome's User-Agent to fool UA sniffing. (#3)" (#31)
    This reverts commit 78bf4e6304fb999db3553daa8f6584ee04ee5953.

--- extension/background.js
+++ extension/background.js
@@ -29,29 +29,3 @@ chrome.webRequest.onBeforeRequest.addListener(details => {
 }, [
 	'blocking'
 ]);
-
-chrome.webRequest.onBeforeSendHeaders.addListener(details => {
-	// xo wants this to be `const`, but that breaks in Firefox.
-	// See https://bugzilla.mozilla.org/show_bug.cgi?id=1269863
-	for (let header of details.requestHeaders) {
-		if (header.name === 'User-Agent') {
-			if (header.value.includes('Firefox') || header.value.includes('Edge')) {
-				header.value = 'Chrome/50';
-			}
-		}
-	}
-
-	return {
-		requestHeaders: details.requestHeaders
-	};
-}, {
-	urls: [
-		'https://mobile.twitter.com/*'
-	],
-	types: [
-		'main_frame'
-	]
-}, [
-	'blocking',
-	'requestHeaders'
-]);
